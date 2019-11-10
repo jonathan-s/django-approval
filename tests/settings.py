71fd4ef4e13345d96b260e9cd0fd87de5803fa19
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-11-10 12:51:51 +0100

    Need to use postgres because of json field

--- tests/settings.py
+++ tests/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_approval',
     }
 }
 
