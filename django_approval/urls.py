635fec15bc06064c97c39b48ec9610fd73d9b67c
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2019-11-15 20:12:57 +0100

    Remove views that are not needed

--- django_approval/urls.py
+++ django_approval/urls.py
@@ -1,35 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
-from django.views.generic import TemplateView
-
-from . import views
-
+from django.conf.urls import url  # noqa
+from django.views.generic import TemplateView  # noqa
 
 app_name = 'django_approval'
-urlpatterns = [
-    url(
-        regex="^Approval/~create/$",
-        view=views.ApprovalCreateView.as_view(),
-        name='Approval_create',
-    ),
-    url(
-        regex="^Approval/(?P<pk>\d+)/~delete/$",
-        view=views.ApprovalDeleteView.as_view(),
-        name='Approval_delete',
-    ),
-    url(
-        regex="^Approval/(?P<pk>\d+)/$",
-        view=views.ApprovalDetailView.as_view(),
-        name='Approval_detail',
-    ),
-    url(
-        regex="^Approval/(?P<pk>\d+)/~update/$",
-        view=views.ApprovalUpdateView.as_view(),
-        name='Approval_update',
-    ),
-    url(
-        regex="^Approval/$",
-        view=views.ApprovalListView.as_view(),
-        name='Approval_list',
-    ),
-	]
+urlpatterns = []
